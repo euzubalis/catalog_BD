c903bbc24bf7306f71b5e186f1599dc7590664d9
euzubalis <ernestasuzubalis4@gmail.com>
2024-07-01 15:21:03 +0300

    Baigiamasis darbas 1 dalis:(sukurtos klasės, admin, pakoreguotas admin puslapis).s

---
+++ mysite/catalog/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from . models import Country, Manufacturer, AirConditioner, TechnicalSpecification
+
+class AirConditionerAdmin(admin.ModelAdmin):
+    list_display = ['manufacturer', 'model_name', 'country', 'power', 'price']
+    list_filter = ['manufacturer', 'country', 'price']
+
+class TechnicalSpecificationAdmin(admin.ModelAdmin):
+    list_display = ['air_conditioner', 'indoor_unit_dimensions', 'outdoor_unit_dimensions', 'energy_efficiency_class', 'operating_temperature']
+    list_filter = ['air_conditioner', 'energy_efficiency_class']
+
+# Register your models here.
+admin.site.register(Country)
+admin.site.register(Manufacturer)
+admin.site.register(AirConditioner, AirConditionerAdmin)
+admin.site.register(TechnicalSpecification, TechnicalSpecificationAdmin)
---
+++ mysite/catalog/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+class Country(models.Model):
+    name = models.CharField(verbose_name="Šalis", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Šalis"
+        verbose_name_plural = "Šalys"
+
+class Manufacturer(models.Model):
+    name = models.CharField(verbose_name="Gamintojas", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Gamintojas"
+        verbose_name_plural = "Gamintojai"
+
+class AirConditioner(models.Model):
+    model_name = models.CharField(verbose_name="Modelis", max_length=100)
+    description = models.TextField(verbose_name="Aprašymas", max_length=1000)
+    power = models.DecimalField(verbose_name="Galingumas (kW)", max_digits=10, decimal_places=2, default=0)
+    price = models.DecimalField(verbose_name="Kaina", max_digits=10, decimal_places=2)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name='air_conditioners', verbose_name="Gamintojas")
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='air_conditioners', verbose_name="Šalis")
+
+    def __str__(self):
+        return self.model_name
+
+    class Meta:
+        verbose_name = "Oro Kondicionierius"
+        verbose_name_plural = "Oro Kondicionieriai"
+
+class TechnicalSpecification(models.Model):
+    air_conditioner = models.OneToOneField(AirConditioner, on_delete=models.CASCADE, related_name='technical_specification', verbose_name="Oro kondicionierius")
+    indoor_unit_dimensions = models.CharField(verbose_name="Vidinio bloko matmenys", max_length=50)
+    outdoor_unit_dimensions = models.CharField(verbose_name="Išorinio bloko matmenys", max_length=50)
+    energy_efficiency_class = models.CharField(verbose_name="Energijos efektyvumo klasė", max_length=10)
+    operating_temperature = models.CharField(verbose_name="Darbinės temperatūros ribos šaldymas/šildymas", max_length=50)
+    outdoor_unit_weight = models.DecimalField(verbose_name="Svoris išorinio bloko (kg)", max_digits=10, decimal_places=2)
+    indoor_unit_weight = models.DecimalField(verbose_name="Svoris vidinio bloko (kg)", max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Technical specs for {self.air_conditioner}"
+
+    class Meta:
+        verbose_name = "Techninė specifikaciją"
+        verbose_name_plural = "Techninės specifikacijos"
