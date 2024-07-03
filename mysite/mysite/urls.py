be17296e3ba15709ade21cca807ac77105afabb1
euzubalis <ernestasuzubalis4@gmail.com>
2024-07-03 15:31:23 +0300

    Testuojam

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls')),
+    path('catalog/', include('catalog.urls')),
+    path("", RedirectView.as_view(url='catalog/', permanent=True))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
