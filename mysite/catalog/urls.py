ae90dab86162820e36d0113a4793b94aab33f11e
euzubalis <ernestasuzubalis4@gmail.com>
2024-07-08 23:18:19 +0300

    + Ideta paieska pagal gamintoja, modeli ir specifikacijas

--- mysite/catalog/urls.py
+++ mysite/catalog/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("airconditioners/<int:pk>/", views.AirConditionerDetailView.as_view(), name='airconditioner'),
     path('technicalspecifications/', TechnicalSpecificationListView.as_view(), name='technicalspecifications'),
     path('technicalspecifications/<int:pk>/', TechnicalSpecificationDetailView.as_view(), name='technicalspecification'),
+    path('search/', views.search, name='search'),
 ]
--- mysite/catalog/views.py
+++ mysite/catalog/views.py
@@ -5,6 +5,7 @@ from django.views import generic
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 # Create your views here.
@@ -34,6 +35,20 @@ def manufacturer(request, manufacturer_id):
     }
     return render(request, template_name="manufacturer.html", context=context)
 
+def search(request):
+    query = request.GET.get('query')
+    manufacturers_search_results = Manufacturer.objects.filter(Q(name__icontains=query) | Q(description__icontains=query) | Q(air_conditioners__technical_specifications__indoor_unit_dimensions__icontains=query) | Q(air_conditioners__technical_specifications__outdoor_unit_dimensions__icontains=query) | Q(air_conditioners__technical_specifications__energy_efficiency_class__icontains=query) | Q(air_conditioners__technical_specifications__operating_temperature__icontains=query) | Q(air_conditioners__technical_specifications__power__icontains=query))
+    airconditioners_search_results = AirConditioner.objects.filter(Q(model_name__icontains=query) | Q(manufacturer__name__icontains=query) | Q(country__name__icontains=query) | Q(technical_specifications__indoor_unit_dimensions__icontains=query) | Q(technical_specifications__outdoor_unit_dimensions__icontains=query) | Q(technical_specifications__energy_efficiency_class__icontains=query) | Q(technical_specifications__operating_temperature__icontains=query) | Q(technical_specifications__power__icontains=query) | Q(technical_specifications__price__icontains=query))
+    technicalspecifications_search_results = TechnicalSpecification.objects.filter(Q(indoor_unit_dimensions__icontains=query) | Q(outdoor_unit_dimensions__icontains=query) | Q(energy_efficiency_class__icontains=query) | Q(operating_temperature__icontains=query) | Q(power__icontains=query) | Q(price__icontains=query))
+    manufacturers_by_model_name = Manufacturer.objects.filter(air_conditioners__model_name__icontains=query)
+    context = {
+        "query": query,
+        "manufacturers": (manufacturers_search_results | manufacturers_by_model_name).distinct(),
+        "airconditioners": airconditioners_search_results,
+        "technicalspecifications": technicalspecifications_search_results,
+    }
+    return render(request, template_name="search.html", context=context)
+
 class AirConditionerListView(generic.ListView):
     model = AirConditioner
     context_object_name = 'airconditioners'
@@ -60,6 +75,8 @@ class TechnicalSpecificationDetailView(DetailView):
         obj = super().get_object(queryset=queryset)
         print(obj.__dict__)  # išvesti objekto duomenis
         return obj
+
+
 
 
 
