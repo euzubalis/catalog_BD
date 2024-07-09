1c9194f140baeb16ee4da490d790ae32091e7098
euzubalis <ernestasuzubalis4@gmail.com>
2024-07-09 18:25:51 +0300

    + Veikia slaptazodzio keitimas

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,6 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-%5vg6z9cwawdqix!(_*kzo+v#(15m)hu$fo$ca_87f%g1m!)pg'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mano_pastas@gmail.com'
+EMAIL_HOST_PASSWORD = 'mano_slaptazodis'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
