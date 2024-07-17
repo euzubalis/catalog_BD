dfb93897cf7c332f24629ca832ac6e60744624e5
euzubalis <ernestasuzubalis4@gmail.com>
2024-07-17 14:14:28 +0300

    +Atlikti vertimai siuose failuose: models.py, base.html, index.html, technicalspecification/s.html, search.html, profile.html, manufacturer/s.html

--- mysite/catalog/models.py
+++ mysite/catalog/models.py
@@ -1,72 +1,73 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.utils.translation import gettext_lazy as _
 
 class Country(models.Model):
-    name = models.CharField(verbose_name="Šalis", max_length=100)
+    name = models.CharField(verbose_name=_("Country"), max_length=100)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "Šalis"
-        verbose_name_plural = "Šalys"
+        verbose_name = _("Country")
+        verbose_name_plural = _("Countries")
 
 class Manufacturer(models.Model):
-    name = models.CharField(verbose_name="Gamintojas", max_length=100)
-    description = models.TextField(verbose_name="Aprašymas", max_length=5000, default="")
-    cover = models.ImageField(verbose_name="Logotipas", upload_to='covers', null=True, blank=True)
+    name = models.CharField(verbose_name=_("Manufacturer"), max_length=100)
+    description = models.TextField(verbose_name=_("Description"), max_length=5000, default="")
+    cover = models.ImageField(verbose_name=_("Logo"), upload_to='covers', null=True, blank=True)
 
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "Gamintojas"
-        verbose_name_plural = "Gamintojai"
+        verbose_name = _("Manufacturer")
+        verbose_name_plural = _("Manufacturers")
 
 class AirConditioner(models.Model):
-    model_name = models.CharField(verbose_name="Modelis", max_length=100)
-    description = models.TextField(verbose_name="Aprašymas", max_length=5000, default="")
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name='air_conditioners', verbose_name="Gamintojas")
-    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='air_conditioners', verbose_name="Šalis")
-    cover = models.ImageField(verbose_name="Nuotrauka", upload_to='covers', null=True, blank=True)
+    model_name = models.CharField(verbose_name=_("Model"), max_length=100)
+    description = models.TextField(verbose_name=_("Description"), max_length=5000, default="")
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name='air_conditioners', verbose_name=_("Manufacturer"))
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='air_conditioners', verbose_name=_("Country"))
+    cover = models.ImageField(verbose_name=_("Photo"), upload_to='covers', null=True, blank=True)
 
     def __str__(self):
         return f'{self.manufacturer} - {self.model_name}'
 
     class Meta:
-        verbose_name = "Oro Kondicionierius"
-        verbose_name_plural = "Oro Kondicionieriai"
+        verbose_name = _("Air Conditioner")
+        verbose_name_plural = _("Air Conditioners")
 
 class TechnicalSpecification(models.Model):
-    air_conditioner = models.ForeignKey(AirConditioner, on_delete=models.CASCADE, related_name='technical_specifications', verbose_name="Oro kondicionierius")
-    indoor_unit_dimensions = models.CharField(verbose_name="Vidinio bloko matmenys", max_length=50)
-    outdoor_unit_dimensions = models.CharField(verbose_name="Išorinio bloko matmenys", max_length=50)
-    energy_efficiency_class = models.CharField(verbose_name="Energijos efektyvumo klasė", max_length=10)
-    operating_temperature = models.CharField(verbose_name="Darbinės temperatūros ribos šaldymas/šildymas", max_length=50)
-    outdoor_unit_weight = models.DecimalField(verbose_name="Svoris išorinio bloko (kg)", max_digits=10, decimal_places=2)
-    indoor_unit_weight = models.DecimalField(verbose_name="Svoris vidinio bloko (kg)", max_digits=10, decimal_places=2)
-    power = models.DecimalField(verbose_name="Galingumas (kW)", max_digits=10, decimal_places=2, default=0)
-    price = models.DecimalField(verbose_name="Kaina", max_digits=10, decimal_places=2, default=0)
-    cover = models.ImageField(verbose_name="Nuotrauka", upload_to='covers', null=True, blank=True)
+    air_conditioner = models.ForeignKey(AirConditioner, on_delete=models.CASCADE, related_name='technical_specifications', verbose_name=_("Air Conditioner"))
+    indoor_unit_dimensions = models.CharField(verbose_name=_("Indoor unit dimensions"), max_length=50)
+    outdoor_unit_dimensions = models.CharField(verbose_name=_("Outdoor unit dimensions"), max_length=50)
+    energy_efficiency_class = models.CharField(verbose_name=_("Energy efficiency class"), max_length=10)
+    operating_temperature = models.CharField(verbose_name=_("Operating temperature"), max_length=50)
+    outdoor_unit_weight = models.DecimalField(verbose_name=_("Outdoor unit weight (kg)"), max_digits=10, decimal_places=2)
+    indoor_unit_weight = models.DecimalField(verbose_name=_("Indoor unit weight (kg)"), max_digits=10, decimal_places=2)
+    power = models.DecimalField(verbose_name=_("Power (kW)"), max_digits=10, decimal_places=2, default=0)
+    price = models.DecimalField(verbose_name=_("Price"), max_digits=10, decimal_places=2, default=0)
+    cover = models.ImageField(verbose_name=_("Cover"), upload_to='covers', null=True, blank=True)
 
     def __str__(self):
         return f"Technininė specifikacija: {self.air_conditioner} {self.power} kW"
 
     class Meta:
-        verbose_name = "Techninė specifikaciją"
-        verbose_name_plural = "Techninės specifikacijos"
+        verbose_name = _("Technical Specification")
+        verbose_name_plural = _("Technical Specifications")
 
 class ConditionerOrder(models.Model):
-    air_conditioner = models.ForeignKey(to="TechnicalSpecification", verbose_name="Techninė specifikacija", on_delete=models.SET_NULL, null=True, blank=True, related_name="orders")
-    client = models.ForeignKey(to=User, verbose_name="Klientas", on_delete=models.SET_NULL, null=True, blank=True)
-    date_created = models.DateTimeField(verbose_name="Data", auto_now_add=True)
-    content = models.TextField(verbose_name="Užsakymas", max_length=5000)
+    air_conditioner = models.ForeignKey(to="TechnicalSpecification", verbose_name=_("Technical specification"), on_delete=models.SET_NULL, null=True, blank=True, related_name="orders")
+    client = models.ForeignKey(to=User, verbose_name=_("Client"), on_delete=models.SET_NULL, null=True, blank=True)
+    date_created = models.DateTimeField(verbose_name=_("Date"), auto_now_add=True)
+    content = models.TextField(verbose_name=_("Order"), max_length=5000)
 
     class Meta:
-        verbose_name = "Užsakymas"
-        verbose_name_plural = "Užsakymai"
+        verbose_name = _("Order")
+        verbose_name_plural = _("Orders")
         ordering = ["-date_created"]
 
 class Profile(models.Model):
