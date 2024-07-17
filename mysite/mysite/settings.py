12aa113721353b4c621174c2fa6de32f85c3172a
euzubalis <ernestasuzubalis4@gmail.com>
2024-07-17 15:49:49 +0300

    +Atlikti vertimai registration.html failuose

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -114,8 +114,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-# LANGUAGE_CODE = 'lt'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'lt'
 
 TIME_ZONE = 'Europe/Vilnius'
 USE_TZ = True
